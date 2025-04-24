d75eba1296a55c14881da8e5aa6ee38586588d3a
Anastasia Shlyk <anastasiashlyk@MacBook-Air-de-Anastasia.local>
2025-04-24 13:02:00 +0200

    algo

---
+++ GSenku.cpp
@@ -0,0 +1,28 @@
+// FUNCIONES / PROCEDIMIENTOS 
+
+// Pre: true
+// Post: lee la configuración y el estado del tablero del fichero de configuración que se le pasa como argumento
+//      inicializando tablero y devolviendo true si todo ha ido bien y false si ha habido algún error
+bool inicializarTablero(const string nombreFichero, tpTablero &tablero);
+
+// Pre: true 
+// Post: lee los movimientos válidos del fichero que se le pasa como argumento 
+//      inicializando la estructura y devolviendo true si todo ha ido bien y false si ha habido algún error
+bool inicializarMovimientosValidos(const string nombreFichero, tpMovimientosValidos &movimientos);
+
+// Pre: tablero contiene el estado actual de la ejecución de la búsqueda de la solución
+// Post: Se ha mostrado el tablero por pantalla
+void mostrarTablero(const tpTablero & tablero);
+
+// Pre: tablero contiene el estado inicial del que se parte para la búsqueda, 
+//      movimientosValidos contiene los movimientos que están permitidos, 
+//      solucionParcial contiene la solución actual como lista de movimientos, En el tablero se han colocada las n primeras piezas de vEntrada, en la columnas indicadas respectivamente en vSalida
+// Post: solucionParcial contendrá la lista de movimientos completa (si no se llega a una solución, estará vacía, numMovs == 0)
+//       Devuelve 1 si encuentra solución, -1 si no la encuentra.
+int buscaSolucion(tpTablero &tablero, const tpMovimientosValidos &movValidos, tpListaMovimientos &solucionParcial, const int retardo=0);
+
+
+// Pre: listaMovimientos contiene la lista de movimientos con la solucion 
+// Post: escribe la lista de movimientos en el fichero que se le pasa como argumento siguiendo el 
+//      formato especificado en el guión (si está vacía, se escribe un -1 en el fichero)
+void escribeListaMovimientos (string nombreFichero, const tpListaMovimientos &solucion); 
