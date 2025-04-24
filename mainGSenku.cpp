e146156ebaec30a8c76399b264bd7c9b6f5f8bf9
Anastasia Shlyk <anastasiashlyk@MacBook-Air-de-Anastasia.local>
2025-04-24 19:03:19 +0200

    Cambios main

--- mainGSenku.cpp
+++ mainGSenku.cpp
@@ -1,7 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "GSenku.hpp"
 using namespace std;
 
 
 
-int main(){
 
+int main(int argc, char* argv[]){
+    tpTablero tablero;
+    tpMovimientosValidos movimientos;
+    string ficheroTablero = argv[1];
+    string ficheroMovimientos = argv[2];
+
+
+    if(inicializarTablero(ficheroTablero, tablero)){
+        if(inicializarMovimientosValidos(ficheroMovimientos, movimientos)){
+
+
+        }
+    }else{
+        cerr<<"Error";
+    }
 }
